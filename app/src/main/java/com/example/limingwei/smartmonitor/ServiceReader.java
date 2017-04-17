8b28f64e0343e7d177a97b1b0c83e821208a859e
limingwei <936961011@qq.com>
2017-04-17 16:13:09 +0800

    4.17

--- app/src/main/java/com/example/limingwei/smartmonitor/ServiceReader.java
+++ app/src/main/java/com/example/limingwei/smartmonitor/ServiceReader.java
@@ -8,10 +8,14 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Binder;
@@ -503,6 +507,7 @@ public class ServiceReader extends Service {
                     .append(",").append(getDate());
 
             mW.write(sb.toString().getBytes());
+            mW.flush();
         } catch (IOException e) {
             notifyError(e);
             return;
@@ -510,6 +515,46 @@ public class ServiceReader extends Service {
     }
 
 
+    private void doStartApplicationWithPackageName(String packagename) {
+
+        // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
+        PackageInfo packageinfo = null;
+        try {
+            packageinfo = getPackageManager().getPackageInfo(packagename, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageinfo == null) {
+            return;
+        }
+
+        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
+
+        // 通过getPackageManager()的queryIntentActivities方法遍历
+        List<ResolveInfo> resolveinfoList = getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+        if (resolveinfo != null) {
+            // packagename = 参数packname
+            String packageName = resolveinfo.activityInfo.packageName;
+            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packagename.mainActivityname]
+            String className = resolveinfo.activityInfo.name;
+            // LAUNCHER Intent
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            // 设置ComponentName参数1:packagename参数2:MainActivity路径
+            ComponentName cn = new ComponentName(packageName, className);
+
+            intent.setComponent(cn);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+    }
 
 
 
@@ -520,6 +565,8 @@ public class ServiceReader extends Service {
             mW.flush();
             mW.close();
             mW = null;
+
+            doStartApplicationWithPackageName("com.android.calendar");
 
             // http://stackoverflow.com/questions/13737261/nexus-4-not-showing-files-via-mtp
 //			MediaScannerConnection.scanFile(this, new String[] { mFile.getAbsolutePath() }, null, null);
